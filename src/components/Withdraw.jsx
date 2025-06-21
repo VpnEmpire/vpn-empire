b740be6a01216215767476bed9d9a214205f30fe
VpnEmpire <zarema.abdurashidova00@mail.ru>
2025-06-21 22:07:48 +0300

    Update Withdraw.jsx

--- src/components/Withdraw.jsx
+++ src/components/Withdraw.jsx
@@ -1,1 +1,16 @@
+// src/components/Withdraw.jsx
+const Withdraw = ({ coins }) => {
+  return (
+    <div className="withdraw-screen">
+      <h2>💸 Вывести</h2>
+      <p className="withdraw-balance">У тебя {coins} монет</p>
+      <button className="withdraw-button" disabled>
+        🔒 Вывести (временно недоступно)
+      </button>
+      <p className="withdraw-note">Функция вывода будет доступна позже</p>
+    </div>
+  );
+};
+
+export default Withdraw;
 
