b7090f148e22d9d4358d76981568b7fda6f9eeca
VpnEmpire <zarema.abdurashidova00@mail.ru>
2025-06-22 16:19:42 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import BottomNav from './components/BottomNav';
-
+import Withdraw from './components/Withdraw';
 function App() {
   const [activeTab, setActiveTab] = useState('home');
   const [coins, setCoins] = useState(() => Number(localStorage.getItem('coins')) || 0);
