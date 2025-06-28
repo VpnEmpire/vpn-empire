36f173f2129698ef0fb3bffc6362e3d0c5550b39
VpnEmpire <zarema.abdurashidova00@mail.ru>
2025-06-28 19:31:33 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import TopTab from './components/Top.jsx';
 import TasksTab from './components/Tasks.jsx';
 import Roulette from './components/Roulette.jsx';
 import Hometab from './components/Home.jsx';
- 
+
 function App() {
   const [activeTab, setActiveTab] = useState('home');
   const [coins, setCoins] = useState(() => Number(localStorage.getItem('coins')) || 0);
