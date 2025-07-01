1cbe07217e0b51b970cda226ff778158862ddaf6
VpnEmpire <zarema.abdurashidova00@mail.ru>
2025-07-01 04:42:20 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -355,22 +355,10 @@ const renderTasks = () => (
  {task.type === 'subscribe' && task.link ? (
   <a href={task.link} target="_blank" rel="noopener noreferrer">
     <button className="task-button">Перейти</button>
-  </a>
-) : (
-  !completedTasks[task.key] && (
-    <button
-      onClick={() => handleTaskClick(task)}
-      disabled={isDisabled}
-      className="task-button"
-    >
-      Выполнить
-    </button>
-  )
-)}
-            
-          {!completedTasks[task.key] && (
+  </a>  
+          !completedTasks[task.key] && (
             <button
-              onClick={() => handleTaskClick(task)}
+              { onClick={() => handleTaskClick(task)}
               disabled={isDisabled}
               className="task-button"
             >
