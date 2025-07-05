2cf4b0f3e1b2dd3be6b2af768bbc83f926c2318e
VpnEmpire <zarema.abdurashidova00@mail.ru>
2025-07-05 06:46:52 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,19 @@
-// db.js
-import sqlite3 from 'sqlite3';
-import { open } from 'sqlite';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
+const fs = require('fs');
 
 const dbPath = path.resolve(__dirname, 'data', 'database.sqlite');
-const db = new sqlite3.Database(dbPath);
+
+if (!fs.existsSync(path.dirname(dbPath))) {
+  fs.mkdirSync(path.dirname(dbPath));
+}
+
+const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) {
+    console.error('Ошибка при подключении к базе данных:', err.message);
+  } else {
+    console.log('✅ База данных подключена');
+  }
+});
 
 module.exports = db;
