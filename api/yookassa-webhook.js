01acac17c338036b980d1fb5720d1284771acc99
VpnEmpire <zarema.abdurashidova00@mail.ru>
2025-07-05 10:45:25 +0300

    Update yookassa-webhook.js

--- api/yookassa-webhook.js
+++ api/yookassa-webhook.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY; // этот ключ для server side!
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default async function handler(req, res) {
