c0940d02471e5fd118c6eed95c4b934353c55923
VpnEmpire <zarema.abdurashidova00@mail.ru>
2025-07-05 07:09:56 +0300

    Update init.sql

--- init.sql
+++ init.sql
@@ -4,3 +4,9 @@ CREATE TABLE IF NOT EXISTS users (
   hasVpnBoost INTEGER DEFAULT 0,
   activateVpn INTEGER DEFAULT 0
 );
+
+CREATE TABLE IF NOT EXISTS completed_tasks (
+  user_id TEXT,
+  task_key TEXT,
+  completed_at INTEGER
+);
