effbc6e65145d296a784055081a74442f66a72a7
VpnEmpire <zarema.abdurashidova00@mail.ru>
2025-07-22 02:32:49 +0300

    Update Top.jsx

--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './styles/Top.css';
+import './styles/Top.css'; 
 import robotImage from './assets/robot-top.png';
 
 const TopTab = () => {
